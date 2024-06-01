8421227325fe27142098fd1ec477b4bd187ceaf6
yoon-gang <sgi07151@naver.com>
2024-06-01 20:05:21 +0900

    Update result.js

--- result.js
+++ result.js
@@ -2,13 +2,15 @@
 
 // 결과를 표시할 HTML 요소를 가져옴
 const resultElement = document.getElementById('result');
+const restartButton = document.getElementById('restart-button');
 
 // 로컬 스토리지에서 저장된 결과를 가져옴
-const mbtiResult = localStorage.getItem('mbtiResult');
+const mbtiResult = localStorage.getItem('mbti_result');
 
 // 결과가 있을 경우 화면에 표시함
 if (mbtiResult) {
-  resultElement.textContent = `당신의 MBTI 유형은 ${mbtiResult} 입니다.`;
+  resultElement.textContent = `${mbtiResult}입니다!`;
 } else {
-  resultElement.textContent = '결과를 찾을 수 없습니다. 다시 시도해 주세요.';
+  resultElement.textContent = '아직 테스트를 진행하지 않았습니다!';
+  restartButton.textContent = '테스트 하러 가기';
 }
